ccb320ff30d5c7bf0c1ad741397d0650d03ab5b0
Iliyan <ilianrusev@gmail.com>
2023-01-07 23:50:30 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RealtorsComponent } from './components/realtors/realtors.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { StoryComponent } from './components/story/story.component';
+import { HomesComponent } from './components/homes/homes.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    HeaderComponent,
+    RealtorsComponent,
+    FeaturesComponent,
+    StoryComponent,
+    HomesComponent,
+    GalleryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/realtors/realtors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-realtors',
+  templateUrl: './realtors.component.html',
+  styleUrls: ['./realtors.component.scss']
+})
+export class RealtorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
