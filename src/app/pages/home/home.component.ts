6618f48405258261a321b48181b38587337439d4
Iliyan <ilianrusev@gmail.com>
2023-01-09 21:20:14 +0200

    features section refactor code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -57,6 +57,39 @@ export class HomeComponent implements OnInit {
     },
   ];
 
+  features: any[] = [
+    {
+      icon: 'assets/img/sprite.svg#icon-global',
+      heading: "World's best luxury homes.",
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.',
+    },
+    {
+      icon: 'assets/img/sprite.svg#icon-trophy',
+      heading: 'Only the best properties.',
+      text: 'Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.',
+    },
+    {
+      icon: 'assets/img/sprite.svg#icon-map-pin',
+      heading: 'All homes in top locations.',
+      text: 'Tenetur distinctio necessitatibus pariatur voluptatibus quidem consequatur harum.',
+    },
+    {
+      icon: 'assets/img/sprite.svg#icon-key',
+      heading: 'New home in one week.',
+      text: 'Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      icon: 'assets/img/sprite.svg#icon-presentation',
+      heading: 'Top 1% realtors.',
+      text: 'Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio necessitatibus pariatur voluptatibus.',
+    },
+    {
+      icon: 'assets/img/sprite.svg#icon-lock',
+      heading: 'Secure payments on Nexter.',
+      text: 'Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia quae.',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
