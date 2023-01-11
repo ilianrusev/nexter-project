e3aed1decb8904872a308a04e95ce47f4457f64c
Iliyan <ilianrusev@gmail.com>
2023-01-11 19:27:09 +0200

    switch to realtors component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RealtorsComponent } from './components/realtors/realtors.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SidebarComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, SidebarComponent, HeaderComponent, RealtorsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
