ad10d5451a2823a42242696dc63544bde9203f37
Iliyan <ilianrusev@gmail.com>
2023-01-11 19:56:07 +0200

    switch to story components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,11 @@ import { FeaturesComponent } from './components/features/features.component';
 import { HomesComponent } from './components/homes/homes.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/story/content/content.component';
+import { PicturesComponent } from './components/story/pictures/pictures.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SidebarComponent, HeaderComponent, RealtorsComponent, FeaturesComponent, HomesComponent, GalleryComponent, FooterComponent],
+  declarations: [AppComponent, HomeComponent, SidebarComponent, HeaderComponent, RealtorsComponent, FeaturesComponent, HomesComponent, GalleryComponent, FooterComponent, ContentComponent, PicturesComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
